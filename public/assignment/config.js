99c13a37cdddfa53dc09c8b17cf7dbea9938888f
nburbine <vladimirgreenbean@yahoo.com>
2016-05-26 13:47:46 -0400

    added default routing

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -8,6 +8,12 @@
             .when("/login", {
                 templateUrl: "views/user/login.view.client.html"
             })
+            .when("/", {
+                templateUrl: "views/user/login.view.client.html"
+            })
+            .when("default", {
+                templateUrl: "views/user/login.view.client.html"
+            })
             .when("/register", {
                 templateUrl: "views/user/register.view.client.html"
             });
