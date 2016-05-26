a62a33ebed3276f16b71c999a048928eb17ea75e
nburbine <vladimirgreenbean@yahoo.com>
2016-05-26 14:25:05 -0400

    used otherwise for default address

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -11,11 +11,11 @@
             .when("/", {
                 templateUrl: "views/user/login.view.client.html"
             })
-            .when("default", {
-                templateUrl: "views/user/login.view.client.html"
-            })
             .when("/register", {
                 templateUrl: "views/user/register.view.client.html"
+            })
+            .otherwise({
+                redirectTo: "/login"
             });
     }
 })();
