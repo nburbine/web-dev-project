e7bc24bfb968ef52b0a30b7df28f8b53bf5230fe
nburbine <vladimirgreenbean@yahoo.com>
2016-05-29 18:16:32 -0400

    added login controller

--- public/assignment/views/user/user.controller.client.js
+++ public/assignment/views/user/user.controller.client.js
@@ -1,3 +1,36 @@
 (function () {
+    angular
+        .module("WebAppMaker")
+        .controller("LoginController", LoginController)
+        .controller("RegisterController", RegisterController)
+        .controller("ProfileController", ProfileController);
     
+    function LoginController($location, UserService) {
+        var vm = this;
+        vm.login = login;
+        
+        function login(username, password) {
+            console.log([username, password]);
+            var user = UserService.findUserByCredentials(username, password);
+            if(user) {
+                console.log('/user/'+user._id);
+                $location.url('/user/' + user._id);
+            } else {
+                vm.alert = "Unable to login";
+            }
+        }
+    }
+    
+    function RegisterController() {
+        var vm = this;
+    }
+    
+    function ProfileController($routeParams, UserService) {
+        var vm = this;
+        vm.userId = $routeParams['userId'];
+        function init() {
+            vm.user = UserService.findUserById(vm.userId);
+        }
+        init();
+    }
 })();
