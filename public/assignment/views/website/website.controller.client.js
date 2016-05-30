00e9fcc508a0f931205ba47062815075a6bdded2
nburbine <vladimirgreenbean@yahoo.com>
2016-05-30 18:34:11 -0400

    implemented edit website controller

--- public/assignment/views/website/website.controller.client.js
+++ public/assignment/views/website/website.controller.client.js
@@ -19,32 +19,59 @@
     
     function NewWebsiteController($routeParams, WebsiteService) {
         var vm = this;
+
         vm.addWebsite = addWebsite;
 
-        console.log('test');
-
         vm.userId = $routeParams['id'];
-
         vm.website = {
             name: "",
             description: ""
         };
+
         function addWebsite() {
             if (vm.website.name.length === 0) {
                 vm.alert = "Please enter website name";
             } else {
                 var result = WebsiteService.createWebsite(vm.userId, vm.website);
-            }
-
-            if (result) {
-                window.location = '#/user/'+vm.userId.toString()+'/website';
-            } else {
-                vm.alert = "Failed to create new website";
+                if (result) {
+                    window.location = '#/user/' + vm.userId.toString() + '/website';
+                } else {
+                    vm.alert = "Failed to create new website";
+                }
             }
         }
     }
     
-    function EditWebsiteController() {
+    function EditWebsiteController($routeParams, WebsiteService) {
+        var vm = this;
+
+        vm.editWebsite = editWebsite;
+        vm.deleteWebsite = deleteWebsite;
         
+        vm.userId = $routeParams['id'];
+        vm.websiteId = $routeParams['wid'];
+        vm.website = angular.copy(WebsiteService.findWebsiteById(vm.websiteId));
+    
+        function editWebsite() {
+            if (vm.website.name.length === 0) {
+                vm.alert = 'Website must have a name';
+            } else {
+                var result = WebsiteService.updateWebsite(vm.websiteId, vm.website);
+                if (result) {
+                    window.location = '#/user/' + vm.userId.toString() + '/website';
+                } else {
+                    vm.alert = 'Failed to update website';
+                }
+            }
+        }
+        
+        function deleteWebsite() {
+            var result = WebsiteService.deleteWebsite(vm.websiteId);
+            if (result) {
+                window.location = '#/user/'+vm.userId.toString()+'/website';
+            } else {
+                vm.alert = 'Failed to delete website';
+            }
+        }
     }
 })();
