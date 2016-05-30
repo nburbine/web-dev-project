553b3a2bb07489951aa66a4c5abdb23defcb06bb
nburbine <vladimirgreenbean@yahoo.com>
2016-05-30 19:45:43 -0400

    implemented new page controlle

--- public/assignment/views/page/page.controller.client.js
+++ public/assignment/views/page/page.controller.client.js
@@ -19,6 +19,29 @@
     
     function NewPageController($routeParams, PageService) {
         var vm = this;
+
+        vm.addPage = addPage;
+
+        vm.userId = $routeParams['id'];
+        vm.websiteId = $routeParams['id'];
+
+        vm.page = {
+            name: "",
+            title: ""
+        };
+
+        function addPage() {
+            if (vm.page.name.length === 0) {
+                vm.alert = 'Page must have a name';
+            } else {
+                var result = PageService.createPage(vm.websiteId, vm.page);
+                if (result) {
+                    window.location = '#/user/'+vm.userId.toString()+'/website/'+vm.websiteId.toString()+'/page';
+                } else {
+                    vm.alert = 'Failed to create page';
+                }
+            }
+        }
     }
     
     function EditPageController($routeParams, PageService) {
