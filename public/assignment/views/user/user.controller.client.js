da1bc894cea9075dd555ad3c02b987d87714b28d
nburbine <vladimirgreenbean@yahoo.com>
2016-05-30 16:35:05 -0400

    implemented RegisterController

--- public/assignment/views/user/user.controller.client.js
+++ public/assignment/views/user/user.controller.client.js
@@ -19,8 +19,28 @@
         }
     }
     
-    function RegisterController() {
+    function RegisterController(UserService) {
         var vm = this;
+        vm.addUser = addUser;
+
+        vm.user = {
+            username: "",
+            password: "",
+            verifyPassword: ""
+        };
+        function addUser() {
+            if (vm.user.username.length === 0 ||
+                vm.user.password.length === 0 ||
+                vm.user.verifyPassword.length === 0) {
+                vm.alert = "Please enter username and passwords";
+            } else if (!(vm.user.password === vm.user.verifyPassword)) {
+                vm.alert = "Passwords don't match";
+            } else if (! (result = UserService.createUser(vm.user))) {
+                vm.alert = "Username taken";
+            } else {
+                window.location = "#/user/" + result.toString();
+            }
+        }
     }
     
     function ProfileController($routeParams, UserService) {
@@ -28,6 +48,7 @@
         vm.updateUser = updateUser;
         
         vm.userId = $routeParams['id'];
+        console.log(vm.userId);
         function init() {
             vm.user = angular.copy(UserService.findUserById(vm.userId));
         }
@@ -35,7 +56,6 @@
 
         function updateUser() {
             var result = UserService.updateUser(vm.userId, vm.user);
-            console.log(result);
             if (result) {
                 vm.success = "User successfully updated";
             } else {
