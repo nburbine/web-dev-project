182f3202a291785d11ed59b168daaf20886b92bf
nburbine <vladimirgreenbean@yahoo.com>
2016-05-30 18:33:12 -0400

    bug fixes

--- public/assignment/services/website.service.client.js
+++ public/assignment/services/website.service.client.js
@@ -18,24 +18,33 @@
             updateWebsite: updateWebsite,
             deleteWebsite: deleteWebsite,
             findWebsitesByUser: findWebsitesByUser,
-            findWebsitesById: findWebsitesById
+            findWebsiteById: findWebsiteById
         };
         return api;
 
         function createWebsite(userId, website) {
             var newWebsite = {
-                _id: (new Date().getTime()),
+                _id: (new Date().getTime()).toString(),
                 name: website.name,
-                developerId: userId
+                developerId: userId,
+                description: website.description
             };
+            if (!newWebsite.description) {
+                newWebsite.description = ""
+            }
             websites.push(newWebsite);
             return true;
         }
 
         function updateWebsite(websiteId, website) {
-            var site = findWebsitesById(websiteId);
-            if (site) {
-                site.name = website.name;
+            var old_website = findWebsiteById(websiteId);
+            if (old_website) {
+                old_website.name = website.name;
+                if (website.description) {
+                    old_website.description = website.description;
+                } else {
+                    old_website.description = "";
+                }
                 return true;
             }
             else {
@@ -44,7 +53,7 @@
         }
 
         function deleteWebsite(websiteId) {
-            var site = findWebsitesById(websiteId);
+            var site = findWebsiteById(websiteId);
             if (site) {
                 websites.splice(websites.indexOf(site), 1);
                 return true;
@@ -64,14 +73,13 @@
             return sites;
         }
 
-        function findWebsitesById(websiteId) {
-            var sites = [];
+        function findWebsiteById(websiteId) {
             for (var i in websites) {
                 if (websites[i]._id === websiteId) {
-                    sites.push(websites[i]);
+                    return websites[i];
                 }
             }
-            return sites;
+            return null;
         }
     }
 })();
