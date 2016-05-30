4ada904d5641bfcad0575ee47e91d6fe6a2b7e7f
nburbine <vladimirgreenbean@yahoo.com>
2016-05-30 13:06:04 -0400

    fixed typo

--- public/assignment/services/website.service.client.js
+++ public/assignment/services/website.service.client.js
@@ -1,6 +1,6 @@
 (function () {
     angular
-        .module("WebAppController")
+        .module("WebAppMaker")
         .factory("WebsiteService", WebsiteService);
 
     function WebsiteService() {
