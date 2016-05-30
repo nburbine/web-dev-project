0786512cabc5870b1d04c6a3cfffb624292d1a36
nburbine <vladimirgreenbean@yahoo.com>
2016-05-30 17:51:41 -0400

    implemented new website controller

--- public/assignment/views/website/website.controller.client.js
+++ public/assignment/views/website/website.controller.client.js
@@ -17,8 +17,31 @@
         console.log(vm.websites);
     }
     
-    function NewWebsiteController() {
-        
+    function NewWebsiteController($routeParams, WebsiteService) {
+        var vm = this;
+        vm.addWebsite = addWebsite;
+
+        console.log('test');
+
+        vm.userId = $routeParams['id'];
+
+        vm.website = {
+            name: "",
+            description: ""
+        };
+        function addWebsite() {
+            if (vm.website.name.length === 0) {
+                vm.alert = "Please enter website name";
+            } else {
+                var result = WebsiteService.createWebsite(vm.userId, vm.website);
+            }
+
+            if (result) {
+                window.location = '#/user/'+vm.userId.toString()+'/website';
+            } else {
+                vm.alert = "Failed to create new website";
+            }
+        }
     }
     
     function EditWebsiteController() {
