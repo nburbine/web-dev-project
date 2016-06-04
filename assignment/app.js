032894b41864226c19dc436db20b8bd31985ccad
nburbine <vladimirgreenbean@yahoo.com>
2016-06-04 17:00:22 -0400

    added app.js

---
+++ assignment/app.js
@@ -0,0 +1,13 @@
+module.exports = function (app) {
+
+    var userService = require("./services/user.service.server.js")(app);
+    // var websiteService = require("./services/website.service.server.js")(app);
+    // var pageService = require("./services/page.service.server.js")(app);
+    // var widgetService = require("./services/widget.service.server.js")(app);
+    
+    app.get("/say/:message", function (req, res) {
+        var msg = req.params["message"];
+        res.send({message: msg});
+        console.log(msg);
+    });
+};
