adda22713ed9182a6dc3b8e32c6abac09f95d731
nburbine <vladimirgreenbean@yahoo.com>
2016-06-04 16:59:52 -0400

    configured client user service to query server

--- public/assignment/services/user.service.client.js
+++ public/assignment/services/user.service.client.js
@@ -3,14 +3,7 @@
         .module("WebAppMaker")
         .factory("UserService", UserService);
 
-    function UserService() {
-        var users = [
-            {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
-            {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
-            {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
-            {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
-        ];
-
+    function UserService($http) {
         var api = {
             createUser: createUser,
             updateUser: updateUser,
@@ -22,70 +15,37 @@
         return api;
 
         function createUser(user) {
-            if (findUserByUsername(user.username)) {
-                return false;
-            } else {
-                var newUser = {
-                    _id: (new Date().getTime()).toString(),
-                    username: user.username,
-                    password: user.password,
-                    firstName: "",
-                    lastName: ""
-                };
-                users.push(newUser);
-                return newUser._id;
-            }
+            var url = "/api/user";
+            var newUser = {
+                username: user.username,
+                password: user.password
+            };
+            return $http.put(url, newUser);
         }
 
         function updateUser(userId, user) {
-            var old_user = findUserById(userId);
-            if (old_user) {
-                old_user.firstName = user.firstName;
-                old_user.lastName = user.lastName;
-                return true;
-            }
-            else {
-                return false;
-            }
+            var url = "/api/user/" + userId;
+            return $http.put(url, user);
         }
 
         function deleteUser(userId) {
-            var user = findUserById(userId);
-            if (user) {
-                users.splice(users.indexOf(user), 1);
-                return true;
-            }
-            else {
-                return false;
-            }
+            var url = "/api/user/"+userId;
+            return $http.delete(url);
         }
 
         function findUserById(userId) {
-            for (var i in users) {
-                if (users[i]._id === userId) {
-                    return users[i];
-                }
-            }
-            return null;
+            var url = "/api/user/"+userId;
+            return $http.get(url);
         }
 
         function findUserByUsername(username) {
-            for (var i in users) {
-                if (users[i].username === username) {
-                    return users[i];
-                }
-            }
-            return false;
+            var url = "/api/user?username="+username;
+            return $http.get(url);
         }
 
         function findUserByCredentials(username, password) {
-            for (var i in users) {
-                if (users[i].username === username &&
-                    users[i].password === password) {
-                    return users[i];
-                }
-            }
-            return false;
+            var url = "/api/user?username="+username+"&password="+password;
+            return $http.get(url);
         }
     }
 })();
