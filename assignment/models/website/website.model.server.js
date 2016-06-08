1aea57b1fd8bcc6c15a7db89787bd1a7d629260e
nburbine <vladimirgreenbean@yahoo.com>
2016-06-08 16:20:12 -0400

    implemented website model server

---
+++ assignment/models/website/website.model.server.js
@@ -0,0 +1,43 @@
+module.exports = function () {
+
+    var WebsiteSchema = require("./website.schema.server.js");
+    var mongoose = require("mongoose");
+    var website = mongoose.model("Website", WebsiteSchema);
+
+    var api = {
+        createWebsite: createWebsite,
+        deleteWebsite: deleteWebsite,
+        updateWebsite: updateWebsite,
+        findWebsiteByUsername: findWebsiteByUsername,
+        findWebsiteByCredentials: findWebsiteByCredentials,
+        findWebsiteById: findWebsiteById
+    };
+    return api;
+    
+    function createWebsiteForUser(userId, website) {
+        return Website.create(Website);
+    }
+    
+    function deleteWebsite(websiteId) {
+        return Website.remove({_id: websiteId});
+    }
+    
+    function updateWebsite(websiteId, website) {
+        return Website.update(
+            {_id: websiteId},
+            {$set:
+                {
+                    firstName: website.firstName,
+                    lastName: website.lastName
+                }}
+        );
+    }
+    
+    function findWebsiteByUsername(websiteId) {
+        return Website.findOne({_id: websiteId});
+    }
+    
+    function findWebsiteById(userId) {
+        return Website.findOne({_id: userId});
+    }
+};
