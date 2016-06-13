fd7cf6c9d921da3f141608091fdbd0b12816f992
nburbine <vladimirgreenbean@yahoo.com>
2016-06-13 11:53:53 -0400

    bug fix

--- assignment/app.js
+++ assignment/app.js
@@ -1,9 +1,11 @@
 module.exports = function (app) {
 
-    var userService = require("./services/user.service.server.js")(app);
-    var websiteService = require("./services/website.service.server.js")(app);
-    var pageService = require("./services/page.service.server.js")(app);
-    var widgetService = require("./services/widget.service.server.js")(app);
+    var models = require("./models/models.js")();
+
+    var userService = require("./services/user.service.server.js")(app, models);
+    var websiteService = require("./services/website.service.server.js")(app, models);
+    var pageService = require("./services/page.service.server.js")(app, models);
+    var widgetService = require("./services/widget.service.server.js")(app, models);
     
     app.get("/say/:message", function (req, res) {
         var msg = req.params["message"];
