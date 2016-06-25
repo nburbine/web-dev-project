c104e2d5d48172a754d1056ca796bc4fbed31b71
zqz <zheng.q@husky.neu.edu>
2016-06-25 17:33:17 -0400

    add user upload image

--- project/services/friend.service.server.js
+++ project/services/friend.service.server.js
@@ -1,12 +1,43 @@
 module.exports = function (app, models) {
     var userModel = models.userModel;
-    
+    var multer = require('multer');
+    var upload = multer({dest: __dirname + '/../../public/uploads'});
     app.get("/projectApi/user/:uid/friend", findFriendsByUserId);
     app.post("/projectApi/user/:uid/friend/:fid", AddFriendById);
     app.get("/projectApi/email/:email", findUserByEmail);
     app.post("/projectApi/friends", getFriends);
+    app.post("/projectApi/upload", upload.single('myFile'), uploadImage);
+
 
     app.delete("/projectApi/user/:uid/friend/:friendId", deleteFriend);
+    function uploadImage(req, res) {
+        var userId = req.body.userId;
+        var width = req.body.width;
+        var myFile = req.file;
+
+        var originalname = myFile.originalname; // file name on user's computer
+        var filename = myFile.filename;     // new file name in upload folder
+        var path = myFile.path;         // full path of uploaded file
+        var destination = myFile.destination;  // folder where file is saved to
+        var size = myFile.size;
+        var mimetype = myFile.mimetype;
+        userModel
+            .uploadImage(userId, "/uploads/" + filename)
+            .then(
+                function (widget) {
+                    res.redirect("/project/#/user/" + userId);
+                },
+                function (error) {
+                    res.status(404).send(error);
+                }
+            );
+    }
+
+
+
+
+
+
     function getFriends(req, res) {
         var friendsList = req.body;
         userModel
