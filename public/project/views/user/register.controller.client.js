6dab04aa23ba06db8c153f6accc65cb502c5d777
zqz <zheng.q@husky.neu.edu>
2016-06-26 15:35:44 -0400

    change header

--- public/project/views/user/register.controller.client.js
+++ public/project/views/user/register.controller.client.js
@@ -9,13 +9,11 @@
 
         vm.register = register;
         function register() {
-            console.log(vm.user);
             if (vm.user.username == null) {
                 vm.error = "Please enter username and password";
             }
             else if (vm.user.password != vm.user.password2) {
-                console.log(password);
-                console.log(password2);
+
                 vm.error = "Verified password incorrect";
             }
             else {
@@ -24,7 +22,6 @@
                     .then(
                         function (response) {
                             var user = response.data;
-                            console.log("registered");
                             $location.url("/user/" + user._id);
                         },
                         function (error) {
